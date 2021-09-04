4b4bafe634083a0ce2fff00470ff4c2ebba1faed
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-09-04 14:15:45 +0100

    Add jaegar client to application

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 import pymongo
 from flask_pymongo import PyMongo
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
 
@@ -9,6 +11,17 @@ app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
+
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 @app.route('/')
 def homepage():
--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
+
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 @app.route('/')
 def homepage():
